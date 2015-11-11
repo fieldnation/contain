9938f9510124c6c8a07918b7a315034cfd535ca5
Jon Pettersson <jon.pettersson@fieldnation.com>
2015-11-11 09:07:05 -0600

    Modified where the cli script is looking for abstract.php

--- scripts/contain-compiler.php
+++ scripts/contain-compiler.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists(__DIR__ . '/../../../autoload.php')) {
-    require_once __DIR__ . '/../../../autoload.php';
+if (file_exists(__DIR__ . '/../../../../cli/abstract.php')) {
+    require_once __DIR__ . '/../../../../cli/abstract.php';
 } else {
     require_once __DIR__ . '/../vendor/autoload.php';
 }
